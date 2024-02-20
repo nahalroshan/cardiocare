e50862ffd8773f3da3aa35aa11fd1b9913315344
Nahal Roshan K <nahalroshan5@gmail.com>
2024-02-20 17:05:22 +0530

    second commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "./components/auth/login";
+import Register from "./components/auth/register";
+import Header from "./components/header";
+import Home from "./components/home";
+import { AuthProvider } from "./contexts/authContext";
+import Prediction from './pages/prediction';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const isMobile = window.innerWidth <= 768;
 
   return (
-    <>
-    <h1 className=''> yhgfhfh hi</h1>
-    </>
-  )
+    <Router>
+      <AuthProvider>
+        <Header />
+        <div className="w-full h-screen flex flex-col bg-gradient-to-r from-white to-sky-300">
+          <Routes>
+            <Route path="/" element={<Login />} /> {/* Set the default route */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/home" element={<Home />} />
+            <Route path='/predict' element={<Prediction />} />
+            
+          </Routes>
+        </div>
+      </AuthProvider>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily:{
+      heading:["Arimo","sans-serif"]
+    }
   },
   plugins: [],
 }
